1484428a019940bc2eba017cb80db806af33a3d9
sf-yuzifu <yuzifu@yuzifu.top>
2024-08-04 00:36:40 +0800

    [fix]修复标题栏闪烁问题

--- src/composables/titleChange.ts
+++ src/composables/titleChange.ts
@@ -8,10 +8,8 @@ export function autoTitleChange() {
   if (location.href.includes('community') || location.href.includes('wiki/forum')) {
     titleChange('论坛 | PICKCAT')
   }
-  else {
-    titleChange('PICKCAT')
-  }
-  if (location.href.includes('work_shop')) {
+
+  else if (location.href.includes('work_shop')) {
     if (Number.parseInt(location.href.slice(25 + 10)) && document.querySelector('.r-work_shop-r-details--details_wrap .r-work_shop-r-details--header_cont .r-work_shop-r-details--title p')) {
       titleChange(`工作室 | ${document.querySelector('.r-work_shop-r-details--details_wrap .r-work_shop-r-details--header_cont .r-work_shop-r-details--title p')!.innerHTML} | PICKCAT`)
     }
@@ -19,10 +17,12 @@ export function autoTitleChange() {
       titleChange('工作室 | PICKCAT')
     }
   }
-  if (location.href.includes('discover')) {
+
+  else if (location.href.includes('discover')) {
     titleChange('发现 | PICKCAT')
   }
-  if (location.href.includes('gallery')) {
+
+  else if (location.href.includes('gallery')) {
     if (Number.parseInt(location.href.slice(25 + 8)) && document.querySelector('.r-gallery-r-detail--name')) {
       titleChange(`活动 | ${document.querySelector('.r-gallery-r-detail--name')!.innerHTML} | PICKCAT`)
     }
@@ -30,18 +30,24 @@ export function autoTitleChange() {
       titleChange('活动 | PICKCAT')
     }
   }
-  if (location.href.includes('user')) {
+
+  else if (location.href.includes('user')) {
     titleChange(`个人中心 | ${document.querySelector('.r-user-c-banner--name')!.innerHTML} | PICKCAT`)
   }
-  if (location.href.includes('work')) {
+
+  else if (location.href.includes('work')) {
     titleChange(`作品 | ${document.querySelector('.r-work-c-work_info--work_name')!.innerHTML} | PICKCAT`)
   }
 
-  if (location.href.includes('studio')) {
+  else if (location.href.includes('studio')) {
     titleChange(`活动 | ${document.querySelector('.r-studio--studio_title')!.innerHTML} | PICKCAT`)
   }
 
-  if (location.href.includes('setting')) {
+  else if (location.href.includes('setting')) {
     titleChange('设置 | PICKCAT')
+  }
+
+  else {
+    titleChange('PICKCAT')
   }
 }
